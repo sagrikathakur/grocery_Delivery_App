430198917dcc084461cdbcb86081d7eb2d8ca555
sagrika thakur <sagrikathakur68@gmail.com>
2026-07-16 19:25:42 +0530

    adding pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
-
+import { LogIn } from 'lucide-react'
+import { Toaster } from 'react-hot-toast'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Toaster position='top-right' toastOptions={{
+        duration: 3000, style: {
+          background: "#1B3022", color: "#fff", borderRadius: "12px", fontSize: "14px"
+        }
+      }} />
+
+
+
+
+      <Routes>
+
+        <Route path='/login' element={<Login />} />
+
+
+
+
+
+      </Routes>
+
+
+
+
+
+
+
+
+
+
+
+    </>
+
+
+
   )
 }
 
