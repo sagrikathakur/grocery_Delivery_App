e6aa8e43e9d9dab86217ebb9241f79c340d1a724
sagrika thakur <sagrikathakur68@gmail.com>
2026-07-19 13:37:49 +0530

    Implemented the hero section layout, styling and responsiveness

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Hero from '../components/Hero'
 
 const Home = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <Hero />
     </div>
   )
