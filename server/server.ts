8b545f359d23def461f4b70a394c012c067f4fb6
sagrika thakur <sagrikathakur68@gmail.com>
2026-07-26 16:24:32 +0530

    added global error handling

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
 
@@ -17,6 +17,19 @@ app.get('/', (req: Request, res: Response) => {
 
 // Routes
 app.use('/api/auth', authRouter);
+
+// error-handlings//
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error("Error:", err);
+  const statusCode = err.statusCode || err.status || 500;
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || "Internal Server Error"
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
